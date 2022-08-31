b1dd9ebfeb954881ddebcf9df23da2de1c0c5cfc
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-08-31 20:06:20 +0200

    feat: :art: Creando middleware personalizado
    MAndar errores si no pasa validaciones

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -2,7 +2,7 @@ const { response } = require('express')
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
-const { validationResult } = require('express-validator');
+
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -29,11 +29,6 @@ const usuariosPut = (req, res = response) => {
 }
 
 const usuariosPost = async (req, res = response) => {
-
-    const errors = validationResult(req);
-    if(!errors.isEmpty() ){
-        return res.status(400).json(errors)
-    }
     
     const {nombre, correo, password, rol} = req.body;
     const usuario = new Usuario({nombre, correo, password, rol});
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const {check} = require('express-validator');
 
+const {validarCampos} = require('../middlewares/validar-campos')
+
 const { 
     usuariosGet, 
     usuariosPost, 
@@ -16,7 +18,11 @@ router.get('/', usuariosGet);
 router.put('/:id',usuariosPut);
 
 router.post('/', [
+    check('nombre', 'El nombre no es válido').not().isEmpty(),
+    check('password', 'El password debe ser de más de 6 letras').isLength({min: 6}),
     check('correo', 'El correo no es válido').isEmail(),
+    check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    validarCampos
 ], usuariosPost);
 
 router.delete('/', usuariosDelete);
