37d80a76e0f6e95a13066499149ea2a4bd1dea8b
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-08-31 19:56:26 +0200

    feat: :art: Validando correos
    Que no se repitan correos y express validator

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -2,6 +2,7 @@ const { response } = require('express')
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
+const { validationResult } = require('express-validator');
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -28,11 +29,19 @@ const usuariosPut = (req, res = response) => {
 }
 
 const usuariosPost = async (req, res = response) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty() ){
+        return res.status(400).json(errors)
+    }
     
     const {nombre, correo, password, rol} = req.body;
     const usuario = new Usuario({nombre, correo, password, rol});
 
     // Verificar si el correo existe
+    const existeEmail = await Usuario.findOne({correo});
+
+    if(existeEmail) return res.status(400).json({msg: "Ya existe el correo"});
 
     // Encriptar la contraseña
     const salt = bcryptjs.genSaltSync();
