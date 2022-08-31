b942f873b21bb07932ced93e41dd497ce15a187b
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-08-31 19:33:31 +0200

    feat: :art: Habilitando post de creación usuario y encriptando contraseña de user

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,7 @@
 const { response } = require('express')
+const bcryptjs = require('bcryptjs');
+
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -24,14 +27,24 @@ const usuariosPut = (req, res = response) => {
     });
 }
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async (req, res = response) => {
     
-    const {nombre, edad} = req.body;
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario({nombre, correo, password, rol});
+
+    // Verificar si el correo existe
+
+    // Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt)
+
+    // Guarda en BD
+
+    await usuario.save();
 
     res.json({ 
         msg: 'post Api -- controlador',
-        nombre,
-        edad
+        usuario
     });
 }
 
