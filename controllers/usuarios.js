74791a098ab304ee48d4a4e2a1f66769122597ff
Ian Mercadal <imercadal@cifpfbmoll.eu>
2022-09-05 19:33:55 +0200

    feat: :art: Creando paginador de Usuarios
    Habilitando filtrado

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -6,11 +6,17 @@ const Usuario = require('../models/usuario');
 const usuariosGet = async (req = request, res = response) => {
 
     const {limite = 5, desde = 0} = req.query;
-    const usuarios = await Usuario.find()
-        .skip(Number(desde) )
-        .limit(Number(limite));
+    const query = {estado: true}
+    
+    const [total, usuarios] = await Promise.all([
+        Usuario.count(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ])
 
     res.json({ 
+        total,
         usuarios
     });
 }
